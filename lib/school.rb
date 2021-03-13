e8f200e1357813541d0ffa7b959f2ec2c915fa00
Rianna Castro <riannaxcastro@gmail.com>
2021-03-12 19:49:40 -1000

    commit tree 979d82e17feb7a98b4dfad81508724630c5f8a01

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :school_name, :roster, :grade
+    
+    
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+        
+        
+    end
+
+    def add_student(name, grade)
+       if @roster[grade]
+        @roster[grade] << name
+       else
+        @roster[grade] = [name]
+       end
+
+
+    end
+
+    def grade(grade)
+        roster[grade]
+
+    end
+
+
+
+
+    def sort
+        new_list = {}
+        @roster.each {|key, value| new_list[key] = value.sort}
+        new_list
+    end
+
+
+end
